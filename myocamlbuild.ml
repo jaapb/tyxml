9a3eee296a66d51f5f0d87a81548e440a1392cc1
Drup <drupyog@zoho.com>
2017-11-17 19:05:58 +0100

    Properly use the standalone deriver.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -45,7 +45,8 @@ let reflect_ppx () =
 
 let tyxml_ppx () =
   let ppx_tyxml = "ppx/ppx_tyxml_standalone."^native_suffix in
-  flag_and_dep [ "ocaml" ; "compile" ; "ppx_tyxml" ] (S [A "-ppx"; P ppx_tyxml])
+  flag_and_dep [ "ocaml" ; "compile" ; "ppx_tyxml" ]
+    (S [A "-ppx"; Quote (S [P ppx_tyxml; Sh "--as-ppx"])])
 
 let () =
   dispatch
--- ppx/ppx_tyxml_standalone.ml
+++ ppx/ppx_tyxml_standalone.ml
@@ -1,3 +1,3 @@
 open Migrate_parsetree
 module M = Ppx_tyxml_register (* dirty way to force link *)
-let () = Driver.run_as_ppx_rewriter ()
+let () = Driver.run_main ()
